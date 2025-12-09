dadb39182f6518684ab3728f74877c2bd0f28351
Gyuwon-Jung <licmm0520@gmail.com>
2025-12-09 14:29:45 +0900

    [feat] UNHAL-7 #3 gatewayFliter 및 blacklist에서 키 가져오게 구성

--- gateway-service/src/main/java/com/amuraedo/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/amuraedo/gatewayservice/GatewayServiceApplication.java
@@ -1,11 +1,15 @@
 package com.amuraedo.gatewayservice;
 
+import com.amuraedo.modulecommon.util.JwtUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
+@Import({JwtUtil.class})
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
---
+++ gateway-service/src/main/java/com/amuraedo/gatewayservice/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,76 @@
+package com.amuraedo.gatewayservice.filter;
+
+import com.amuraedo.gatewayservice.util.GatewayRedisUtil;
+import com.amuraedo.modulecommon.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    private final JwtUtil jwtUtil;
+    private final GatewayRedisUtil redisUtil;
+
+    public static class Config {}
+
+    @Value("${internal.secret-key}")
+    private String internalSecretKey;
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            // 헤더 유무 확인
+            if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
+                return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authHeeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+            String token = authHeeader.replace("Bearer ", "");
+
+            if(!jwtUtil.validateToken(token)){
+                return onError(exchange, "Invalid Token", HttpStatus.UNAUTHORIZED);
+            }
+
+            return redisUtil.hasBlackList(token)
+                    .flatMap(isBlackListed -> {
+                        if(isBlackListed){
+                            return onError(exchange, "Logout Token (Blacklist)", HttpStatus.UNAUTHORIZED);
+                        }
+
+                        String userId = jwtUtil.getUserId(token);
+                        String role = jwtUtil.getRole(token);
+
+                        ServerHttpRequest modifiedRequest = request.mutate()
+                                .header("X-User-Id", userId)
+                                .header("X-User-Role", role)
+                                .header("X-Internal-Secret", internalSecretKey)
+                                .build();
+                        return chain.filter(exchange.mutate().request(modifiedRequest).build());
+                    });
+
+
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String message, HttpStatus status) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(status);
+        log.error("Gateway Filter Error: {}", message);
+        return response.setComplete();
+    }
+
+}
