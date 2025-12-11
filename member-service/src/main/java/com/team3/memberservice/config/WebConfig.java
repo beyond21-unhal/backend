6ee22cf4e0cbfe9a7749f3aaeb3005f5bdfdeb27
Gyuwon-Jung <licmm0520@gmail.com>
2025-12-11 22:01:03 +0900

    [refactor] UNHAL-18 #17 interceptor 걸리는 위치 수정

--- member-service/src/main/java/com/team3/memberservice/config/WebConfig.java
+++ member-service/src/main/java/com/team3/memberservice/config/WebConfig.java
@@ -15,10 +15,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(internalSecretInterceptor)
-                .addPathPatterns("/api/member") // 모든 API 검사
-                .excludePathPatterns("/api/auth/**"); // (선택) 로그인은 게이트웨이 필터 안 거칠 수도 있으니 예외 처리 필요할 수도 있음
-        // 보통은 /api/auth/** 도 게이트웨이를 통한다면 검사하는 게 맞습니다.
-        // internal secret은 게이트웨이가 무조건 붙여주니까요.
+                .addPathPatterns("/member")
+                .excludePathPatterns("/auth/**");
     }
 
 }
