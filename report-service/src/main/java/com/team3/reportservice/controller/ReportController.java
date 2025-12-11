92c88ee9674fb9131b0bb31f393d5c0cc148d062
rlatjddms <kseo_o@naver.com>
2025-12-11 14:48:16 +0900

    [refactor] UNHAL-14 #11 리포트 조회 시 예외 처리 추가

--- report-service/src/main/java/com/team3/reportservice/controller/ReportController.java
+++ report-service/src/main/java/com/team3/reportservice/controller/ReportController.java
@@ -2,6 +2,7 @@ package com.team3.reportservice.controller;
 
 import com.team3.reportservice.dto.request.CreateReportDTO;
 import com.team3.reportservice.dto.response.ApiResponse;
+import com.team3.reportservice.dto.response.ReportViewDTO;
 import com.team3.reportservice.service.ReportService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/report")
@@ -23,7 +25,7 @@ public class ReportController {
     @PostMapping("/last-week-report")
     @Operation(summary = "지난 주 리포트 생성 API입니다.")
     @SecurityRequirement(name = "JWT")
-    public ResponseEntity<ApiResponse<?>> createLastWeekReport(
+    public ResponseEntity<ApiResponse<String>> createLastWeekReport(
             @RequestBody CreateReportDTO dto,
             @Parameter(hidden = true) @RequestHeader("X-User-Id") Long userId
     ) {
@@ -35,9 +37,16 @@ public class ReportController {
     @GetMapping("/all-report")
     @Operation(summary = "사용자별 전체 리포트 조회 API입니다.")
     @SecurityRequirement(name = "JWT")
-    public ResponseEntity<ApiResponse<?>> userViewReport(
+    public ResponseEntity<ApiResponse<List<ReportViewDTO>>> userViewReport(
             @Parameter(hidden = true) @RequestHeader("X-User-Id") Long userId
     ) {
+        List<ReportViewDTO> reports = reportService.getReportsByUserId(userId);
+
+        if (reports.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .body(ApiResponse.success(null));
+        }
+
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ApiResponse.success(reportService.getReportsByUserId(userId)));
     }
@@ -45,10 +54,16 @@ public class ReportController {
     @GetMapping("/search-report/{date}")
     @Operation(summary = "날짜 검색을 통한 리포트 조회 API입니다.")
     @SecurityRequirement(name = "JWT")
-    public ResponseEntity<ApiResponse<?>> searchViewReport(
+    public ResponseEntity<ApiResponse<ReportViewDTO>> searchViewReport(
             @Parameter(hidden = true) @RequestHeader("X-User-Id") Long userId,
             @PathVariable("date") LocalDate date
     ) {
+        ReportViewDTO report = reportService.getReportByDate(userId, date);
+
+        if (report == null) {
+            return ResponseEntity.noContent().build();
+        }
+
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ApiResponse.success(reportService.getReportByDate(userId, date)));
     }
@@ -56,7 +71,7 @@ public class ReportController {
     @DeleteMapping("/delete/{reportId}")
     @Operation(summary = "리포트 삭제 API입니다.")
     @SecurityRequirement(name = "JWT")
-    public ResponseEntity<ApiResponse<?>> deleteReport(
+    public ResponseEntity<ApiResponse<String>> deleteReport(
             @Parameter(hidden = true)  @RequestHeader("X-User-Id") Long userId,
             @PathVariable("reportId") Long reportId
     ) {
